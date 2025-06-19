9272022829e344eeddf0d126ea4015d70ee66ecf
chriskitso <kitsomwakayeni@gmail.com>
2025-06-20 01:41:03 +0200

    Fix: Added real app pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
+// next.config.ts
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;

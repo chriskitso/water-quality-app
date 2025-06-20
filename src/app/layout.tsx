746bfd775652a41d7d0bad9dedf1449d282f26b4
chriskitso <kitsomwakayeni@gmail.com>
2025-06-20 02:05:55 +0200

    Fix: remove unused font variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,15 +5,7 @@ import "./globals.css";
 
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Water Quality Monitor",
